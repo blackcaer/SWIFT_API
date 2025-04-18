7e8f99d46fdd779e136dd733591d82db1bd88ffe
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2025-04-19 00:08:19 +0200

    feat: Enhance SwiftCodeBase model with field descriptions and constraints

--- swift-api/app/models.py
+++ swift-api/app/models.py
@@ -2,20 +2,27 @@ from typing import Optional
 from sqlmodel import Field, SQLModel
 
 
-# Model testowy
 class Message(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     content: str
 
 
-# Modele dla SWIFT codes
 class SwiftCodeBase(SQLModel):
-    swiftCode: str = Field(primary_key=True)
-    bankName: str
-    address: str
-    countryISO2: str = Field(max_length=2)
-    countryName: str
-    isHeadquarter: bool = False
+    swiftCode: str = Field(
+        primary_key=True,
+        max_length=11,
+        description="SWIFT code (8 or 11 chars)"
+    )
+    
+    bankName: str = Field(max_length=100) 
+    address: str = Field(max_length=250)  
+    countryISO2: str = Field(
+        max_length=2,
+        index=True,
+        description="ISO 2-letter country code"
+    )
+    countryName: str = Field(max_length=60)
+    isHeadquarter: bool = Field(default=False)
 
 
 class SwiftCode(SwiftCodeBase, table=True):
